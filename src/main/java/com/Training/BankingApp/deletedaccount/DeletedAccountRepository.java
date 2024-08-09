7437799776936cb148b4f4789419d38a991f11f7
AshharAli-18 <iashharali.18@gmail.com>
2024-08-09 13:50:41 +0500

    Delete src directory

--- src/main/java/com/Training/BankingApp/deletedaccount/DeletedAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Training.BankingApp.deletedaccount;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeletedAccountRepository extends JpaRepository<DeletedAccount, Long> {
-
-}
--- src/main/java/com/Training/BankingApp/user/CustomAuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.Training.BankingApp.user;
-
-public class CustomAuthenticationException extends RuntimeException {
-    public CustomAuthenticationException(String message) {
-        super(message);
-    }
-}
